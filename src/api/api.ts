892535d8e7d87e1bc23fb38608cf3f8ef7040f77
zkzk8953 <aoao1023@gmail.com>
2022-12-09 17:52:16 +0900

    221209 axios 모듈화 및 인터셉터 설정

---
+++ src/api/api.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosInstance } from "axios";
+
+type CustomResponseError = {
+  result: boolean;
+  message: string;
+}
+
+const api: AxiosInstance = axios.create({
+  baseURL: "https://pre-onboarding-selection-task.shop",
+  headers: {
+    "Content-Type": "application/json",
+    token: "",
+  },
+  timeout: 5000,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    // 요청 전에 수행할 코드
+
+    return config;
+  },
+  (error) => {
+    // 오류 요청을 보내기 전 수행할 코드
+
+    return Promise.reject(error)
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    // 응답 전에 수행할 코드
+
+    return response;
+  },
+  (error) => {
+    // 오류 응답을 보내기 전 수행할 코드
+    const customError: CustomResponseError = {
+      result: true,
+      message: error.response.data.message
+    }
+
+    return Promise.reject(customError);
+  },
+);
+
+export default api;
